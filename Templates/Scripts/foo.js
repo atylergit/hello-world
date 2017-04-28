c087086b207461868681b364f9f8f7b915f492e7
Anthony Tyler <atyler@ewingirrigation.com>
2017-04-28 10:17:17 -0700

    Adding a dynamic Ajax command layer

--- Templates/Scripts/foo.js
+++ Templates/Scripts/foo.js
@@ -8,3 +8,10 @@ $(document).on('click', "#DoSomethingButton", function () {
         $("#buttonResults").html('</br><p>'+ result.friendlyText +'</p>');
     });
 });
+
+$(document).on('click', "#TriggerErrorButton", function () {
+    jQuery.post("Ajax/ajaxGateway.php", {command:'triggerError'}, function(data){
+        var result = JSON.parse(data);
+        $("#buttonResults").html('</br><p>'+ result.friendlyText +'</p>');
+    });
+});
