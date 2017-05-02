76340bbaf26542e2d296d6b2a587d94780aa656b
Anthony Tyler <atyler@ewingirrigation.com>
2017-05-02 13:12:46 -0700

    Adding a dynamic Ajax command layer

--- Templates/Scripts/foo.js
+++ Templates/Scripts/foo.js
@@ -42,12 +42,12 @@ function doAjaxCall(form) {
     jQuery.post("Ajax/ajaxGateway.php", postData, function(data){
         var result = JSON.parse(data);
         if (result.status == 'error') {
-            $("#buttonResults").html('<div><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>'+ result.friendlyText +'</div>');
+            $("#buttonResults").html('<div class="fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>'+ result.friendlyText +'</div>');
             $("#buttonResults > div").removeClass();
             $("#buttonResults > div").addClass('alert alert-danger alert-dismissable');
             console.log(result);
         } else {
-            $("#buttonResults").html('<div><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>' + result.friendlyText + '</div>');
+            $("#buttonResults").html('<div class="fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>' + result.friendlyText + '</div>');
             $("#buttonResults > div").removeClass();
             $("#buttonResults > div").addClass('alert alert-success alert-dismissable');
             console.log(result.data);
