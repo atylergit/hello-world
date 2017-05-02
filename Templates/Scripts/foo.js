3a86d49168d90fe1a536356c2f2115554e9e7aa4
Anthony Tyler <atyler@ewingirrigation.com>
2017-05-02 13:07:38 -0700

    Adding a dynamic Ajax command layer

--- Templates/Scripts/foo.js
+++ Templates/Scripts/foo.js
@@ -23,7 +23,7 @@ $(document).on('submit', "form", function (event) {
     event.preventDefault();
 });
 
-$(".alert button.close").click(function (e) {
+$(".alert a.close").click(function (e) {
     $(this).parent().fadeOut('slow');
 });
 
@@ -42,12 +42,12 @@ function doAjaxCall(form) {
     jQuery.post("Ajax/ajaxGateway.php", postData, function(data){
         var result = JSON.parse(data);
         if (result.status == 'error') {
-            $("#buttonResults").html('<div><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>'+ result.friendlyText +'</div>').fadeIn('slow');
+            $("#buttonResults").html('<div><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>'+ result.friendlyText +'</div>');
             $("#buttonResults > div").removeClass();
             $("#buttonResults > div").addClass('alert alert-danger alert-dismissable');
             console.log(result);
         } else {
-            $("#buttonResults").html('<div><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>' + result.friendlyText + '</div>').fadeIn('slow');
+            $("#buttonResults").html('<div><a href="#" class="close" data-dismiss="alert" aria-label="close">&#215;</a>' + result.friendlyText + '</div>');
             $("#buttonResults > div").removeClass();
             $("#buttonResults > div").addClass('alert alert-success alert-dismissable');
             console.log(result.data);
